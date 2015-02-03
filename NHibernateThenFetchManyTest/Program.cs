fb8dd9a0d92918ffbc5958a3e68311371aa30d69
Thomas Lazar <thomas.lazar@isoware.com>
2015-02-03 09:24:24 +0100

    simplification

--- NHibernateThenFetchManyTest/Program.cs
+++ NHibernateThenFetchManyTest/Program.cs
@@ -28,34 +28,45 @@ namespace NHibernateThenFetchManyTest
                 {
                     // create a couple of Stores each with some Components and Employees
                     var bigMachine = new MaintenanceObject {Name = "Great big Machine"};
-                    var smallMachine = new MaintenanceObject {Name = "Smaller Machine"};
+                    //var smallMachine = new MaintenanceObject {Name = "Smaller Machine"};
 
-                    var cog = new Component {Name = "cog", Price = 3.60};
-                    var screw = new Component {Name = "screw", Price = 4.49};
-                    var torque = new Component {Name = "torque", Price = 0.79};
-                    var couplings = new Component {Name = "couplings", Price = 1.29};
-                    var hinge = new Component {Name = "hinge", Price = 2.10};
-                    var toggle = new Component {Name = "toggle", Price = 2.41};
+                    var cog = new Component {Name = "cog", Price = 3.60, MaintenanceObject = bigMachine};
+                    bigMachine.Components.Add(cog);
+                    var screw = new Component { Name = "screw", Price = 4.49, MaintenanceObject = bigMachine };
+                    bigMachine.Components.Add(screw);
+                    var torque = new Component { Name = "torque", Price = 0.79, MaintenanceObject = bigMachine };
+                    bigMachine.Components.Add(torque);
+                    var couplings = new Component { Name = "couplings", Price = 1.29, MaintenanceObject = bigMachine };
+                    bigMachine.Components.Add(couplings);
+                    //var hinge = new Component { Name = "hinge", Price = 2.10, MaintenanceObject = smallMachine };
+                    //smallMachine.Components.Add(hinge);
+                    //var toggle = new Component { Name = "toggle", Price = 2.41, MaintenanceObject = smallMachine };
+                    //smallMachine.Components.Add(toggle);
 
-                    var job1 = new MaintenanceJob {JobNumber = "#1", JobInfo = "Harrison"};
-                    var job2 = new MaintenanceJob {JobNumber = "#2", JobInfo = "Torrance"};
-                    var job3 = new MaintenanceJob {JobNumber = "#3", JobInfo = "Walkters"};
-                    var job4 = new MaintenanceJob {JobNumber = "#4", JobInfo = "Taft"};
-                    var job5 = new MaintenanceJob {JobNumber = "#5", JobInfo = "Pope"};
+                    var job1 = new MaintenanceJob {JobNumber = "#1", JobInfo = "Harrison", MaintenanceObject = bigMachine};
+                    bigMachine.Jobs.Add(job1);
+                    var job2 = new MaintenanceJob { JobNumber = "#2", JobInfo = "Torrance", MaintenanceObject = bigMachine };
+                    bigMachine.Jobs.Add(job2);
+                    var job3 = new MaintenanceJob { JobNumber = "#3", JobInfo = "Walkters", MaintenanceObject = bigMachine };
+                    bigMachine.Jobs.Add(job3);
+                    //var job4 = new MaintenanceJob { JobNumber = "#4", JobInfo = "Taft", MaintenanceObject = smallMachine };
+                    //smallMachine.Jobs.Add(job4);
+                    //var job5 = new MaintenanceJob { JobNumber = "#5", JobInfo = "Pope", MaintenanceObject = smallMachine };
+                    //smallMachine.Jobs.Add(job5);
 
                     // add componentses to the stores, there's some crossover in the componentses in each
                     // MaintenanceObject, because the MaintenanceObject-Component relationship is many-to-many
-                    AddComponentsToMaintenanceObject(bigMachine, cog, screw, torque, couplings, hinge);
-                    AddComponentsToMaintenanceObject(smallMachine, couplings, hinge, toggle);
+                    //AddComponentsToMaintenanceObject(bigMachine, cog, screw, torque, couplings, hinge);
+                    //AddComponentsToMaintenanceObject(smallMachine, couplings, hinge, toggle);
 
                     // add maintenanceJobs to the stores, this relationship is a one-to-many, so one
                     // MaintenanceJob can only work at one MaintenanceObject at a time
-                    AddMaintenanceJobsToMaintenanceObject(bigMachine, job1, job2, job3);
-                    AddMaintenanceJobsToMaintenanceObject(smallMachine, job4, job5);
+                    //AddMaintenanceJobsToMaintenanceObject(bigMachine, job1, job2, job3);
+                    //AddMaintenanceJobsToMaintenanceObject(smallMachine, job4, job5);
 
                     // save both stores, this saves everything else via cascading
                     session.SaveOrUpdate(bigMachine);
-                    session.SaveOrUpdate(smallMachine);
+                    //session.SaveOrUpdate(smallMachine);
 
                     transaction.Commit();
                 }
@@ -71,8 +82,25 @@ namespace NHibernateThenFetchManyTest
                     //    WriteStorePretty(MaintenanceObject);
                     //}
 
-                    var jobs = session.Query<MaintenanceJob>().Fetch(x => x.MaintenanceObject).ThenFetchMany(x => x.Components).ToList();
+                    var jobs = session.Query<MaintenanceJob>()
+                        .Fetch(x => x.MaintenanceObject)
+                        .ThenFetchMany(x => x.Components)
+                        .ToList();
 
+
+                    foreach (var job in jobs)
+                    {
+                        Console.WriteLine("Job Number: {0}", job.JobNumber);
+                        Console.WriteLine("  MxObject: {0}", job.MaintenanceObject.Name);
+                        Console.WriteLine("    Components:");
+                        ;
+                        foreach (var component in job.MaintenanceObject.Components)
+                        {
+                            Console.WriteLine("      {0}", component.Name);
+                            
+                        }
+                        Console.WriteLine("----");
+                    }
                 }
 
                 Console.ReadKey();
@@ -123,7 +151,10 @@ namespace NHibernateThenFetchManyTest
                         .UsingFile("firstProject.db")
                 )
                 .Mappings(m =>
-                    m.FluentMappings.AddFromAssemblyOf<Program>())
+                {
+                    m.FluentMappings.AddFromAssemblyOf<Program>();
+                    m.FluentMappings.ExportTo(@"C:\temp\mappings");
+                })
                 .ExposeConfiguration(BuildSchema)
                 .BuildSessionFactory();
         }
